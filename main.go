e8947f7728dac97292598862899f72df9c535530
Abhi Yerra <abhi@berkeley.edu>
2016-10-04 03:28:19 +0000

    http

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Println("Starting dotplan...")
+	log.Println("Starting journlr...")
 
-	startSMTP()
+	r := mux.Router()
+
+	r.Handle("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Hack Sonoma"))
+	}).Method("GET")
+
+	http.ListenAndServe(":6891", handlers.LoggingHandler(os.Stdout, r))
+
 }
